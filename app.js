5a4db1aa80c5156f2ce559a4976ae45774c8894c
trevor-r-allen <tallen2699@gmail.com>
2020-12-09 17:03:28 -0700

    added spock and lizard to choices

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const choices = {
-  rock: {beats:"scissors"},
-  paper: {beats:"rock"},
-  scissors: {beats:"paper"}
+  rock: ["scissors", "lizard"],
+  paper: ["rock", "spock"],
+  scissors: ["paper", "lizard"],
+  spock: ["rock", "scissors"],
+  lizard: ["spock", "paper"]
 }
 
 function play(playerChoice) {
   let computerChoice = randomChoice();console.log(computerChoice);
-  console.log(choices[computerChoice].beats);
+  console.log(choices[computerChoice].includes(playerChoice));
   if(playerChoice == computerChoice) {
     document.getElementById("resultMessage").textContent = "Equilibrium :|"
     return alert("Computer chose " + computerChoice + ", it's a DRAW!")
   }
-  else if(playerChoice == choices[computerChoice].beats) {
+  else if(choices[computerChoice].includes(playerChoice)) {
     document.getElementById("resultMessage").textContent = "Condolences :("
     return alert("Computer chose " + computerChoice + ", YOU LOSE!")
   }
@@ -23,3 +25,12 @@ function randomChoice(){
   let choiceArr = Object.keys(choices)
   return choiceArr[Math.floor(Math.random() * choiceArr.length)]
 }
+
+function drawButtons(){
+  let choiceArr = Object.keys(choices)
+  let buttons = ''
+  choiceArr.forEach(choice => {buttons += `<button class="panel-body btn btn-outline-primary m-3" onclick="play('${choice}')">${choice.toUpperCase()}</button>`})
+
+  document.getElementById('buttons').innerHTML = buttons
+}
+drawButtons()
